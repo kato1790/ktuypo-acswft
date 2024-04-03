2bc98ad6e588628cfad30e8a0688b91f616a4c2d
kato1790 <160192310+kato1790@users.noreply.github.com>
2024-04-03 17:51:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ktuypo-acswft
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kato1790/ktuypo-acswft)
---
+++ index.ts
@@ -0,0 +1,10 @@
+
+import { ColorPicker } from '@syncfusion/ej2-inputs';
+
+/**
+ * Default ColorPicker sample
+ */
+
+    
+    let colorPicker: ColorPicker = new ColorPicker({}, '#color-picker');
+
